2df87ad54247babf3c76a5c371f0c18464a6eedd
Pedro Lucas <pedrogmobile2@gmail.com>
2021-09-16 20:39:10 -0300

    Atualizando os valores das constantes da testagem

--- testing.h
+++ testing.h
@@ -1,9 +1,9 @@
 #include "structRB.h"
 
 #define RAND_LIMITER 100000 // Valor para limitar o rand()
-#define QTD_RN 1000 // Quantidade de arvores RN
-#define QTD_NODES 10000 // Quantiadde de nos em cada arvore
-#define QTD_REMOVES 1000 // Quantidade de remocoes
+#define QTD_RN 10 // Quantidade de arvores RN
+#define QTD_NODES 1000 // Quantiadde de nos em cada arvore
+#define QTD_REMOVES 450 // Quantidade de remocoes
 #define PRINT_LIMIT 10 // Limite para imprimir as arvores
 
 // TESTAGEM
